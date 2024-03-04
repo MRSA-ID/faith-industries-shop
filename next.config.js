6eb1f80102ed2d26cf90e5c7361607ceba69fb74
MRSA <sanggisda@gmail.com>
2024-03-04 21:00:25 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withPlausibleProxy } = require("next-plausible");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = withPlausibleProxy()(nextConfig);

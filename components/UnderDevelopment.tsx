b22f3b087f57e8266ab5115d86befaafec6075a4
MRSA <sanggisda@gmail.com>
2024-03-07 16:08:07 +0700

    update: component underdevelopment class name

--- components/UnderDevelopment.tsx
+++ components/UnderDevelopment.tsx
@@ -1,19 +1,19 @@
 "use client";
-import react from 'react';
+import react, { FC } from 'react';
 import Image from 'next/image'
+import Wrench from '@/public/wrench.jpg'
 
 export const UnderDevelopment: FC = function () {
   return (
-    <div class="rounded-lg flex flex-col justify-center items-center h-full">
-      <div class="flex flex-col items-center justify-center gap-6">
+    <div className="rounded-lg flex flex-col justify-center items-center h-full">
+      <div className="flex flex-col items-center justify-center gap-6">
         <Image
-          src="/wrench.jpg"
-          width={160}
-          height={160}
+          src={Wrench}
           alt="Wrench"
           className="rounded-2xl"
+          priority
         />
-        <p class="text-bs-black-1 text-base text-center font-bold w-[280px] dark:text-white">
+        <p className="text-bs-black-1 text-base text-center font-bold w-[280px] dark:text-white">
           Layanan ini masih dalam tahap pengembangan
         </p>
       </div>

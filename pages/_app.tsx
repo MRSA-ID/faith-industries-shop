7a716ef98f374da869d0425a962de609da829337
MRSA <sanggisda@gmail.com>
2024-03-08 16:28:41 +0700

    update: can animation on change pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { motion, AnimatePresence } from "framer-motion"
 import PlausibleProvider from "next-plausible";
 import { Flowbite, ThemeModeScript } from "flowbite-react";
 import { Inter } from "next/font/google";
@@ -11,6 +12,7 @@ import { twMerge } from "tailwind-merge";
 import { flowbiteTheme } from "../ts/theme";
 import "../styles/globals.css";
 import type { NextPageWithLayout } from '@/ts/NextPageWithLayout'
+import { useRouter } from "next/router";
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
@@ -26,6 +28,8 @@ const inter = Inter({ subsets: ["latin"] });
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const [queryClient] = React.useState(() => new QueryClient());
   const getLayout = Component.getLayout || ((page) => page);
+  const router = useRouter();
+
   return (
     <>
       <div 
@@ -37,7 +41,27 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
         <ReactQueryDevtools />
         <PlausibleProvider domain={NEXT_PUBLIC_WEBSITE_DOMAIN}>
           <Flowbite theme={{ theme: flowbiteTheme }}>
-            {getLayout(<Component {...pageProps} />)}
+            {getLayout(
+              <AnimatePresence mode="wait" initial={true}>
+              <motion.div key={router.pathname}>
+                <Component {...pageProps} />
+              </motion.div>
+              <motion.div
+                className="slide-in bg-gray-50 dark:bg-gray-900"
+                initial={{scaleY:0}}
+                animate={{scaleY:0}}
+                exit={{scaleY: 1}}
+                transition={{duration:1, ease: [0.22, 1, 0.36, 1]}}
+              ></motion.div>
+              <motion.div
+                className="slide-out bg-gray-50 dark:bg-gray-900"
+                initial={{scaleY:1}}
+                animate={{scaleY:0}}
+                exit={{scaleY: 0}}
+                transition={{duration:1, ease: [0.22, 1, 0.36, 1]}}
+              ></motion.div>
+              </AnimatePresence>
+            )}
           </Flowbite>
         </PlausibleProvider>
       </QueryClientProvider>

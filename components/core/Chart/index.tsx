8e788ec1dc6cc5bf5f8fed0335e965d226d1d7a6
MRSA <sanggisda@gmail.com>
2024-03-08 16:27:14 +0700

    update: chart

--- components/core/Chart/index.tsx
+++ components/core/Chart/index.tsx
@@ -1,34 +1,86 @@
 "use client";
 import react, { FC, HTMLProps, useState } from 'react';
 import dynamic from "next/dynamic";
+import { ApexOptions } from "apexcharts"
 const ApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-export const ChartComponent: FC<HTMLProps<HTMLDivElement>> = function ({className}) {
-  const [state, setState] = useState(
-    {
-      options: {
-        chart: {
-          id: "basic-bar"
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-        }
-      },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
-        }
-      ]
+const defaultOptions = {
+  chart: {
+    id: "basic-bar"
+  },
+  xaxis: {
+    labels:{
+      style:{
+        colors:['#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF']
+      }
+    },
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  },
+  yaxis:{
+    labels:{
+      style:{
+        colors:['#9CA3AF']
+      }
     }
-  ) 
+  },
+}
+
+const defultSeries = [
+  {
+    name: "series-1",
+    data: [30, 40, 45, 50, 49, 60, 70, 91]
+  }
+]
+
+interface propsChart extends HTMLProps<HTMLDivElement> {
+  typeChart?: 'line'| 'area' | 'bar' | 'boxPlot' | 'rangeBar' | 'rangeArea' | 'treemap' | 'donut' | 'radialBar',
+  options?: ApexOptions,
+  series?: any
+}
+
+export const ChartComponent: FC<propsChart> = function ({
+  typeChart = 'bar',
+  options = defaultOptions,
+  series = defultSeries,
+  className
+}:propsChart) {
+  // const [state, setState] = useState(
+  //   {
+  //     // options: {
+  //     //   chart: {
+  //     //     id: "basic-bar"
+  //     //   },
+  //     //   xaxis: {
+  //     //     labels:{
+  //     //       style:{
+  //     //         colors:['#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF','#9CA3AF']
+  //     //       }
+  //     //     },
+  //     //     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  //     //   },
+  //     //   yaxis:{
+  //     //     labels:{
+  //     //       style:{
+  //     //         colors:['#9CA3AF']
+  //     //       }
+  //     //     }
+  //     //   },
+  //     // },
+  //     series: [
+  //       {
+  //         name: "series-1",
+  //         data: [30, 40, 45, 50, 49, 60, 70, 91]
+  //       }
+  //     ]
+  //   }
+  // ) 
   return (
     <>
       <ApexChart 
         className={className}
-        options={state.options}
-        series={state.series}
-        type="bar"
+        options={options}
+        series={series}
+        type={typeChart}
         width="100%"
         height="100%"
       />

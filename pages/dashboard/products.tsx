018f750ee06861baa7fd58c526e28dc555ace8e0
MRSA <sanggisda@gmail.com>
2024-03-11 14:34:57 +0700

    add: table in product dashboard

--- pages/dashboard/products.tsx
+++ pages/dashboard/products.tsx
@@ -1,8 +1,8 @@
 import dynamic from 'next/dynamic'
 import type { ReactElement } from 'react'
-import { HomePageContent } from './homepage-content'
 import Head from 'next/head'
-import { UnderDevelopment } from '@/components/UnderDevelopment'
+import { Button, Checkbox, Table } from 'flowbite-react'
+import { HiOutlinePlusSm, HiOutlineRefresh } from 'react-icons/hi'
 // import DashboardLayout from '@/components/dashboard/DashboardLayout'
 const DashboardLayout = dynamic(() => import('@/components/dashboard/DashboardLayout'), { ssr: false })
 
@@ -12,7 +12,132 @@ const Products = () => {
       <Head>
         <title>Stock Product | FTH Product</title>
       </Head>
-      <UnderDevelopment />
+      
+      <div className='p-4'>
+        <header>
+          <h2 className="mb-6 text-2xl font-semibold dark:text-white">
+            All products
+          </h2>
+        </header>
+        <div className='mb-5 flex justify-between'>
+          {/* Component input search */}
+          <form className="w-full max-w-md">   
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+          </form>
+          {/* End Component input search */}
+
+          <div className="flex flex-wrap gap-2 items-end">
+            <Button color='blue' className='h-10'>
+              <HiOutlineRefresh className="mr-2 h-5 w-5" />
+              Refresh
+            </Button>
+            <Button color='blue' className='h-10'>
+              <HiOutlinePlusSm className="mr-2 h-5 w-5" />
+              Add product
+            </Button>
+          </div>
+
+        </div>
+        <div className="overflow-x-auto">
+          <Table hoverable>
+            <Table.Head className='text-gray-500' >
+              <Table.HeadCell className="bg-gray-200 dark:bg-gray-700 p-4">
+                <Checkbox className='checked:text-blue-600 focus:ring-blue-300 dark:focus:ring-blue-600' />
+              </Table.HeadCell>
+              <Table.HeadCell className='bg-gray-200 dark:bg-gray-700'>Product name</Table.HeadCell>
+              <Table.HeadCell className='bg-gray-200 dark:bg-gray-700'>Color</Table.HeadCell>
+              <Table.HeadCell className='bg-gray-200 dark:bg-gray-700'>Category</Table.HeadCell>
+              <Table.HeadCell className='bg-gray-200 dark:bg-gray-700'>Price</Table.HeadCell>
+              <Table.HeadCell className='bg-gray-200 dark:bg-gray-700'>
+                <span className="sr-only">Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="p-4">
+                  <Checkbox className='checked:text-blue-600 focus:ring-blue-300 dark:focus:ring-blue-600' />
+                </Table.Cell>
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {'Apple MacBook Pro 17"'}
+                </Table.Cell>
+                <Table.Cell>Sliver</Table.Cell>
+                <Table.Cell>Laptop</Table.Cell>
+                <Table.Cell>$2999</Table.Cell>
+                <Table.Cell>
+                  <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="p-4">
+                  <Checkbox className='checked:text-blue-600 focus:ring-blue-300 dark:focus:ring-blue-600' />
+                </Table.Cell>
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  Microsoft Surface Pro
+                </Table.Cell>
+                <Table.Cell>White</Table.Cell>
+                <Table.Cell>Laptop PC</Table.Cell>
+                <Table.Cell>$1999</Table.Cell>
+                <Table.Cell>
+                  <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="p-4">
+                  <Checkbox className='checked:text-blue-600 focus:ring-blue-300 dark:focus:ring-blue-600' />
+                </Table.Cell>
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
+                <Table.Cell>Black</Table.Cell>
+                <Table.Cell>Accessories</Table.Cell>
+                <Table.Cell>$99</Table.Cell>
+                <Table.Cell>
+                  <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                    Edit
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+          <nav className="flex  w-full items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
+            <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span className="font-semibold text-gray-900 dark:text-white">1000</span></span>
+            <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                <li>
+                  <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                </li>
+                <li>
+                  <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                </li>
+                <li>
+                  <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                </li>
+                <li>
+                  <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                </li>
+                <li>
+                  <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                </li>
+                <li>
+                  <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                </li>
+                <li>
+                  <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   )
 }

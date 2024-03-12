e42fedc094911cc1066a6bbe2c4df648c177bdbb
MRSA <sanggisda@gmail.com>
2024-03-12 15:31:34 +0700

    fix style react paginate

--- components/dashboard/DashboardPagination/index.tsx
+++ components/dashboard/DashboardPagination/index.tsx
@@ -93,13 +93,12 @@ const DashboardPagination: FC<DashboardPaginationProps> = ({
         previousLabel={'Previous'}
         renderOnZeroPageCount={null as any}
         containerClassName="pg-container inline-flex -space-x-px text-sm h-8"
-        previousLinkClassName="pg-item flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border !border-blue-100 rounded-s-lg
-        hover:bg-blue-100 hover:text-blue-700 hover:border-blue-100 dark:bg-gray-800 dark:!border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        previousLinkClassName="pg-item flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border !border-blue-100 rounded-s-lg hover:bg-blue-100 hover:text-blue-700 hover:border-blue-100 dark:bg-gray-800 dark:!border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
         nextLinkClassName="pg-item flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-blue-100 rounded-e-lg hover:bg-blue-100 hover:text-blue-700 hover:border-blue-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
         pageLinkClassName="pg-item flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-blue-100 hover:bg-blue-100 hover:text-blue-700 hover:border-blue-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-        activeLinkClassName="flex items-center justify-center px-3 h-8 !text-blue-600 border border-blue-100 bg-blue-100 border-blue-100 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:!bg-gray-700 dark:!text-white"
-        disabledLinkClassName="cursor-not-allowed bg-blue-100 text-blue-500 dark:!bg-gray-700 dark:!text-white dark:hover:bg-gray-700"
-        breakClassName="pg-break flex items-center justify-center px-3 h-8 leading-tight bg-white border border-blue-100 bg-blue-100 hover:bg-blue-100 hover:border-blue-100 hover:text-blue-700 !text-blue-600 bg-blue-50 dark:bg-gray-800 dark:border-gray-700 dark:!text-gray-400 dark:hover:bg-gray-700 dark:!bg-gray-700 dark:hover:text-white"
+        activeLinkClassName="!text-blue-600 border border-blue-100 !bg-blue-100 border-blue-100 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:!bg-gray-700 dark:!text-white"
+        disabledLinkClassName="cursor-not-allowed !bg-blue-100 !text-blue-500 dark:!bg-gray-700 dark:!text-white dark:hover:bg-gray-700"
+        breakClassName="pg-break block flex items-center justify-center px-3 h-8 leading-tight border border-blue-100 bg-blue-100 hover:bg-blue-100 hover:border-blue-100 hover:text-blue-700 !text-blue-600 bg-blue-50 dark:bg-gray-800 dark:border-gray-700 dark:!text-gray-400 dark:hover:bg-gray-700 dark:!bg-gray-700 dark:hover:text-white"
         onPageChange={(page) => onPageClick(page.selected + 1)}
         forcePage={current - 1}
       />

d81eb26a7c722aa45c6d940877708e9d3d76ff83
MRSA <sanggisda@gmail.com>
2024-03-12 15:39:31 +0700

    fix style react paginate

--- components/dashboard/DashboardPagination/index.tsx
+++ components/dashboard/DashboardPagination/index.tsx
@@ -98,7 +98,7 @@ const DashboardPagination: FC<DashboardPaginationProps> = ({
         pageLinkClassName="pg-item flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-blue-100 hover:bg-blue-100 hover:text-blue-700 hover:border-blue-100 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
         activeLinkClassName="!text-blue-600 border border-blue-100 !bg-blue-100 border-blue-100 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:!bg-gray-700 dark:!text-white"
         disabledLinkClassName="cursor-not-allowed !bg-blue-100 !text-blue-500 dark:!bg-gray-700 dark:!text-white dark:hover:bg-gray-700"
-        breakClassName="pg-break block flex items-center justify-center px-3 h-8 leading-tight border border-blue-100 bg-blue-100 hover:bg-blue-100 hover:border-blue-100 hover:text-blue-700 !text-blue-600 bg-blue-50 dark:bg-gray-800 dark:border-gray-700 dark:!text-gray-400 dark:hover:bg-gray-700 dark:!bg-gray-700 dark:hover:text-white"
+        breakClassName="pg-break block flex items-center justify-center px-3 h-8 leading-tight border border-blue-100  hover:bg-blue-100 bg-white hover:border-blue-100 hover:text-blue-700 !text-blue-600 dark:border-gray-700 dark:!text-gray-400 dark:hover:bg-gray-700 dark:bg-gray-800 dark:hover:text-white"
         onPageChange={(page) => onPageClick(page.selected + 1)}
         forcePage={current - 1}
       />
